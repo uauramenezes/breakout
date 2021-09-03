4a3a503311984bf4fa103df43f3cef80b59a9fc0
uauramenezes <uaurasilva@hotmail.com>
2021-09-02 22:15:41 -0300

    Add players

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,66 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Pong".to_owned(),
+        fullscreen: false,
+        window_height: 500,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+struct Player {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+    color: Color,
+}
+
+impl Player {
+    fn draw(&self) {
+        draw_rectangle(self.x, self.y, self.w, self.h, self.color)
+    }
+
+    fn moves(&mut self, up: KeyCode, down: KeyCode) {
+        if is_key_down(up) {
+            self.y -= 5.0
+        }
+        if is_key_down(down) {
+            self.y += 5.0
+        }
+    }
+}
+
+fn new_player(side: &str) -> Player {
+    let x = if side == "left" {
+        11.0
+    } else {
+        screen_width() - 11.0 - 8.0
+    };
+    Player {
+        x: x,
+        y: screen_height() / 2.0 - 80.0 / 2.0,
+        w: 8.0,
+        h: 80.0,
+        color: WHITE,
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    let mut left_player = new_player("left");
+    let mut right_player = new_player("right");
+
     loop {
-        clear_background(RED);
+        clear_background(BLACK);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        left_player.draw();
+        right_player.draw();
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        left_player.moves(KeyCode::W, KeyCode::S);
+        right_player.moves(KeyCode::Up, KeyCode::Down);
 
         next_frame().await
     }
