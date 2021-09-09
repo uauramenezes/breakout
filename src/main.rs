d971dbe81f1f745cf3142509a25c89a85f2102df
uauramenezes <uaurasilva@hotmail.com>
2021-09-09 20:41:14 -0300

    Add player and ball with movement

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,12 @@ use macroquad::prelude::*;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Pong".to_owned(),
-        window_width: 500,
-        window_height: 800,
+        window_width: 450,
+        window_height: 650,
         fullscreen: false,
         window_resizable: false,
         ..Default::default()
     }
-}
-
-// TODO: Change game to Breakout
-
-enum Side {
-    Left,
-    Right,
 }
 
 struct Player {
@@ -25,72 +18,82 @@ struct Player {
     h: f32,
     dir: f32,
     speed: f32,
-    color: Color,
 }
 
-impl Player {
-    fn draw(&self) {
-        draw_rectangle(self.x, self.y, self.w, self.h, self.color)
-    }
+struct Ball {
+    x: f32,
+    y: f32,
+    r: f32,
+    dir_x: f32,
+    dir_y: f32,
+    speed: f32,
+}
 
-    fn player_dir(&mut self) {
-        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
-            self.dir = -1.0;
-        } else if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
-            self.dir = 1.0;
-        } else {
-            self.dir = 0.0;
-        }
-    }
+impl Ball {
+    fn move_self(&mut self, play: bool) {
+        self.speed = match play {
+            true => 150.0,
+            false => 0.0,
+        };
 
-    fn ai_dir(&mut self) {
-        if self.y <= 0.0 || self.y + self.h >= screen_height() {
-            self.dir = -self.dir;
+        if self.x - self.r <= 0.0 || self.x + self.r >= screen_width() {
+            self.dir_x = -self.dir_x;
         }
-    }
+        if self.y - self.r <= 0.0 || self.y + self.r >= screen_height() {
+            self.dir_y = -self.dir_y;
+        }
 
-    fn move_self(&mut self, side: Side) {
-        match side {
-            Side::Left => self.player_dir(),
-            Side::Right => self.ai_dir(),
-        }
-        self.y += self.speed * self.dir;
+        self.y += self.speed * self.dir_y * get_frame_time();
+        self.x += self.speed * self.dir_x * get_frame_time();
     }
 }
 
-fn new_player(side: Side) -> Player {
-    let x = match side {
-        Side::Left => 11.0,
-        Side::Right => screen_width() - 11.0 - 8.0,
-    };
-    let speed = match side {
-        Side::Left => 5.0,
-        Side::Right => 3.0,
-    };
-    Player {
-        x: x,
-        y: screen_height() / 2.0 - 80.0 / 2.0,
-        w: 8.0,
-        h: 80.0,
-        dir: 1.0,
-        speed: speed,
-        color: WHITE,
+fn get_player_dir() -> f32 {
+    if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+        return -1.0;
     }
+    if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+        return 1.0;
+    }
+    return 0.0;
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut left_player = new_player(Side::Left);
-    let mut right_player = new_player(Side::Right);
+    let mut player = Player {
+        x: screen_width() / 2.0 - 40.0,
+        y: screen_height() - 25.0,
+        w: 80.0,
+        h: 8.0,
+        dir: 0.0,
+        speed: 350.0,
+    };
+
+    let mut ball = Ball {
+        x: screen_width() / 2.0,
+        y: screen_height() - 25.0 - 8.0,
+        r: 8.0,
+        dir_x: -1.0,
+        dir_y: -1.0,
+        speed: 0.0,
+    };
+
+    let mut play = false;
 
     loop {
         clear_background(BLACK);
 
-        left_player.draw();
-        right_player.draw();
+        draw_rectangle(player.x, player.y, player.w, player.h, WHITE);
+        draw_circle(ball.x, ball.y, ball.r, WHITE);
 
-        left_player.move_self(Side::Left);
-        right_player.move_self(Side::Right);
+        if is_key_down(KeyCode::Space) && !play {
+            play = true;
+        }
+
+        ball.move_self(play);
+
+        player.dir = get_player_dir();
+        player.x += player.speed * player.dir * get_frame_time();
 
         next_frame().await
     }
