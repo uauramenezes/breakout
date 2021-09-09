eafd4bdb22328ecd9ac089e3a9ab9a1d92fa05f5
uauramenezes <uaurasilva@hotmail.com>
2021-09-09 19:41:48 -0300

    Change game to breakout

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use macroquad::prelude::*;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Pong".to_owned(),
+        window_width: 500,
+        window_height: 800,
         fullscreen: false,
-        window_height: 500,
         window_resizable: false,
         ..Default::default()
     }
 }
+
+// TODO: Change game to Breakout
 
 enum Side {
     Left,
@@ -20,6 +23,8 @@ struct Player {
     y: f32,
     w: f32,
     h: f32,
+    dir: f32,
+    speed: f32,
     color: Color,
 }
 
@@ -28,13 +33,28 @@ impl Player {
         draw_rectangle(self.x, self.y, self.w, self.h, self.color)
     }
 
-    fn moves(&mut self, up: KeyCode, down: KeyCode) {
-        if is_key_down(up) {
-            self.y -= 5.0
+    fn player_dir(&mut self) {
+        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+            self.dir = -1.0;
+        } else if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+            self.dir = 1.0;
+        } else {
+            self.dir = 0.0;
         }
-        if is_key_down(down) {
-            self.y += 5.0
+    }
+
+    fn ai_dir(&mut self) {
+        if self.y <= 0.0 || self.y + self.h >= screen_height() {
+            self.dir = -self.dir;
         }
+    }
+
+    fn move_self(&mut self, side: Side) {
+        match side {
+            Side::Left => self.player_dir(),
+            Side::Right => self.ai_dir(),
+        }
+        self.y += self.speed * self.dir;
     }
 }
 
@@ -43,11 +63,17 @@ fn new_player(side: Side) -> Player {
         Side::Left => 11.0,
         Side::Right => screen_width() - 11.0 - 8.0,
     };
+    let speed = match side {
+        Side::Left => 5.0,
+        Side::Right => 3.0,
+    };
     Player {
         x: x,
         y: screen_height() / 2.0 - 80.0 / 2.0,
         w: 8.0,
         h: 80.0,
+        dir: 1.0,
+        speed: speed,
         color: WHITE,
     }
 }
@@ -63,8 +89,8 @@ async fn main() {
         left_player.draw();
         right_player.draw();
 
-        left_player.moves(KeyCode::W, KeyCode::S);
-        right_player.moves(KeyCode::Up, KeyCode::Down);
+        left_player.move_self(Side::Left);
+        right_player.move_self(Side::Right);
 
         next_frame().await
     }
