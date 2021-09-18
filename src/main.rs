f058f55996d217f1c205d14fed3f31c478b4ad14
uauramenezes <uaurasilva@hotmail.com>
2021-09-17 22:47:26 -0300

    Add ball collision with brick

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,24 @@ impl Brick {
     }
 
     // TODO: Add collision
-    // fn collision(&self, ball: &Ball) {}
-
-    // TODO: Add Endgame
+    fn collision(&self, ball: &Ball) -> bool {
+        if (ball.x >= self.x
+            && ball.x <= self.x + self.w
+            && ball.y + ball.r >= self.y
+            && ball.y - ball.r <= self.y + self.h)
+            || (((ball.x - ball.r <= self.x + self.w
+                && ball.x >= self.x + self.w
+                && ball.x_direction > 0.0)
+                || (ball.x + ball.r >= self.x
+                    && ball.x - ball.r <= self.x
+                    && ball.x_direction < 0.0))
+                && ball.y - ball.r <= self.y + self.h
+                && ball.y + ball.r >= self.y)
+        {
+            return true;
+        }
+        false
+    }
 }
 
 #[derive(Clone)]
@@ -38,24 +53,24 @@ impl Wall {
     fn new() -> Vec<Brick> {
         let mut wall: Vec<Brick> = Vec::new();
 
-        let color = vec![RED, GREEN, BLUE, YELLOW, VIOLET, GRAY, ORANGE, LIME, BEIGE];
-        for i in 0..9 {
+        let color = vec![RED, GREEN, BLUE, YELLOW, VIOLET, ORANGE];
+        for i in 0..6 {
             for j in 0..5 {
                 let c = i + j;
                 let c = match c {
-                    9 => 0,
-                    10 => 1,
-                    11 => 2,
-                    12 => 3,
+                    6 => 0,
+                    7 => 1,
+                    8 => 2,
+                    9 => 3,
                     _ => i + j,
                 };
-                let x = (i * 50 + 15) as f32;
-                let y = ((j + 1) * 10) as f32;
+                let x = (i * 80 + 15) as f32;
+                let y = ((j + 1) * 30) as f32;
                 wall.push(Brick {
                     x: x,
                     y: y,
                     w: 50.0,
-                    h: 10.0,
+                    h: 15.0,
                     color: color[c],
                 });
             }
@@ -127,15 +142,25 @@ struct Ball {
     speed: f32,
 }
 
+fn get_random_number() -> f32 {
+    let random_number = rand::gen_range(1, 3);
+    let random = match random_number {
+        1 => -1.0,
+        2 => 1.0,
+        _ => 0.0,
+    };
+    random
+}
+
 impl Ball {
     fn new() -> Ball {
         Ball {
             x: screen_width() / 2.0,
-            y: screen_height() - 25.0 - 8.0,
+            y: screen_height() - 25.0 - 8.5,
             r: 8.0,
-            x_direction: -1.0,
+            x_direction: get_random_number(),
             y_direction: -1.0,
-            speed: 250.0,
+            speed: 15.0,
         }
     }
 
@@ -143,14 +168,22 @@ impl Ball {
         draw_circle(self.x, self.y, self.r, WHITE);
     }
 
-    fn move_self(&mut self, player: &Player) {
+    fn move_self(&mut self, player: &Player, brick: &Brick) {
         if (self.x - self.r < 0.0 && self.x_direction < 0.0)
             || (self.x + self.r > screen_width() && self.x_direction > 0.0)
             || (((self.x - self.r <= player.x + player.w && self.x >= player.x + player.w)
-                || (self.x >= player.x && self.x - self.r < player.x))
+                || (self.x + self.r >= player.x && self.x <= player.x))
                 && self.y + self.r >= player.y
                 && self.y - self.r <= player.y + player.h
                 && self.y_direction > 0.0)
+            || (((self.x - self.r <= brick.x + brick.w
+                && self.x >= brick.x + brick.w
+                && self.x_direction < 0.0)
+                || (self.x + self.r >= brick.x
+                    && self.x - self.r <= brick.x
+                    && self.x_direction > 0.0))
+                && self.y - self.r <= brick.y + brick.h
+                && self.y + self.r >= brick.y)
         {
             self.x_direction = -self.x_direction;
         }
@@ -160,6 +193,10 @@ impl Ball {
                 && self.y + self.r >= player.y
                 && self.x <= player.x + player.w
                 && self.y - self.r <= player.y + player.h)
+            || (self.x >= brick.x
+                && self.y + self.r >= brick.y
+                && self.x <= brick.x + brick.w
+                && self.y - self.r <= brick.y + brick.h)
         {
             self.y_direction = -self.y_direction;
         }
@@ -167,10 +204,11 @@ impl Ball {
         self.y += self.speed * self.y_direction * get_frame_time();
         self.x += self.speed * self.x_direction * get_frame_time();
     }
-
     fn restart(&mut self) {
         self.x = screen_width() / 2.0;
-        self.y = screen_height() - 25.0 - 8.0;
+        self.y = screen_height() - 25.0 - 8.5;
+        self.x_direction = get_random_number();
+        self.y_direction = -1.0;
     }
 }
 
@@ -187,7 +225,7 @@ fn play_game(mut play: bool) -> bool {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let wall = Wall::new();
+    let mut wall = Wall::new();
     let mut player = Player::new();
 
     let mut ball = Ball::new();
@@ -204,18 +242,26 @@ async fn main() {
 
         play = play_game(play);
 
-        for brick in wall.clone().into_iter() {
+        for (index, brick) in wall.clone().iter().enumerate() {
             brick.draw();
-        }
-
-        if play {
-            ball.move_self(&player);
+            if play {
+                ball.move_self(&player, &brick);
+            }
+            let brick_collision = brick.collision(&ball);
+            if brick_collision {
+                wall.remove(index);
+            }
         }
 
         if ball.y > screen_height() {
             play = false;
             ball.restart();
             player.restart();
+            wall = Wall::new();
+        }
+
+        if wall.len() == 0 {
+            play = false;
         }
 
         next_frame().await
